da6390ab6f595c36490e1e6cca7ed04435d157a2
aleksejnovalov910-pixel <aleksejnovalov910@gmail.com>
2026-08-19 16:54:04 +0300

    Wire Veloria client modules

--- VELORIA-RP/src/client/index.ts
+++ VELORIA-RP/src/client/index.ts
@@ -1,4 +1,8 @@
 import { Events } from '../shared/events';
+import './controls';
+import './vehicles';
+import './hud';
+import './character/creator';
 
 let authBrowser: BrowserMp | null = null;
 
@@ -17,30 +21,10 @@ function closeAuth() {
 
 mp.events.add('playerReady', openAuth);
 
-mp.events.add('veloria:cef:login', (username: string, password: string) => {
-  mp.events.callRemote(Events.AuthLogin, username, password);
-});
-
-mp.events.add('veloria:cef:register', (username: string, password: string) => {
-  mp.events.callRemote(Events.AuthRegister, username, password);
-});
-
-mp.events.add(Events.AuthResult, (success: boolean, message: string) => {
-  authBrowser?.execute(`window.veloriaAuthResult?.(${JSON.stringify(success)}, ${JSON.stringify(message)})`);
-});
-
-mp.events.add(Events.CharacterList, (json: string) => {
-  authBrowser?.execute(`window.veloriaCharacterList?.(${JSON.stringify(json)})`);
-});
-
-mp.events.add('veloria:cef:character:create', (slot: number, firstName: string, lastName: string, appearance: string) => {
-  mp.events.callRemote(Events.CharacterCreate, slot, firstName, lastName, appearance);
-});
-
-mp.events.add('veloria:cef:character:select', (characterId: number) => {
-  mp.events.callRemote(Events.CharacterSelect, characterId);
-});
-
-mp.events.add(Events.CharacterSpawned, () => {
-  closeAuth();
-});
+mp.events.add('veloria:cef:login', (username: string, password: string) => mp.events.callRemote(Events.AuthLogin, username, password));
+mp.events.add('veloria:cef:register', (username: string, password: string) => mp.events.callRemote(Events.AuthRegister, username, password));
+mp.events.add(Events.AuthResult, (success: boolean, message: string) => authBrowser?.execute(`window.veloriaAuthResult?.(${JSON.stringify(success)}, ${JSON.stringify(message)})`));
+mp.events.add(Events.CharacterList, (json: string) => authBrowser?.execute(`window.veloriaCharacterList?.(${JSON.stringify(json)})`));
+mp.events.add('veloria:cef:character:create', (slot: number, firstName: string, lastName: string, appearance: string) => mp.events.callRemote(Events.CharacterCreate, slot, firstName, lastName, appearance));
+mp.events.add('veloria:cef:character:select', (characterId: number) => mp.events.callRemote(Events.CharacterSelect, characterId));
+mp.events.add(Events.CharacterSpawned, closeAuth);
