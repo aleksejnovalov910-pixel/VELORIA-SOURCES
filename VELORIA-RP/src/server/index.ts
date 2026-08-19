df0a9e6a38ccb3efbe76c2c81d5b52f222937805
aleksejnovalov910-pixel <aleksejnovalov910@gmail.com>
2026-08-19 16:43:19 +0300

    Add RAGE MP server bootstrap

---
+++ VELORIA-RP/src/server/index.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import { logger } from './core/logger';
+import { initMySql } from './database/mysql';
+import { initRedis } from './database/redis';
+import { registerAccountModule } from './modules/accounts';
+import { registerCharacterModule } from './modules/characters';
+
+async function bootstrap() {
+  logger.info('Starting VELORIA RP v0.1 core...');
+  await initMySql();
+  await initRedis();
+
+  registerAccountModule();
+  registerCharacterModule();
+
+  mp.events.add('playerJoin', (player: PlayerMp) => {
+    player.dimension = 1000 + player.id;
+    logger.info(`Player connected: ${player.name} (${player.id})`);
+  });
+
+  mp.events.add('playerQuit', (player: PlayerMp, exitType: string, reason: string) => {
+    logger.info(`Player disconnected: ${player.name}; ${exitType}; ${reason}`);
+  });
+
+  logger.info('VELORIA RP core initialized');
+}
+
+void bootstrap().catch((error) => {
+  logger.error('Fatal bootstrap error', error);
+  process.exitCode = 1;
+});
