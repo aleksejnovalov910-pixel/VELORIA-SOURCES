b6ee1f293b2c9f4b98866a6033230841a271cc34
aleksejnovalov910-pixel <aleksejnovalov910@gmail.com>
2026-08-19 18:13:10 +0300

    feat: bind inventory key

--- VELORIA-RP/src/client/controls/index.ts
+++ VELORIA-RP/src/client/controls/index.ts
@@ -3,4 +3,5 @@ import { VELORIA_CONFIG } from '../../shared/config/server';
 
 mp.keys.bind(VELORIA_CONFIG.phoneKey, true, () => mp.events.call(VeloriaEvents.PhoneToggle));
 mp.keys.bind(VELORIA_CONFIG.tabletKey, true, () => mp.events.call(VeloriaEvents.TabletToggle));
+mp.keys.bind(VELORIA_CONFIG.inventoryKey, true, () => mp.events.call(VeloriaEvents.InventoryToggle));
 mp.keys.bind(VELORIA_CONFIG.settingsKey, true, () => mp.events.call(VeloriaEvents.SettingsToggle));
