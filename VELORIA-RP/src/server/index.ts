230fa8ed7c90b973f4b245e46bf633fb6decbab3
aleksejnovalov910-pixel <aleksejnovalov910@gmail.com>
2026-08-19 17:46:42 +0300

    feat: register gameplay UI bridge

--- VELORIA-RP/src/server/index.ts
+++ VELORIA-RP/src/server/index.ts
@@ -8,32 +8,6 @@ import { registerGameplayModules } from './modules/gameplay';
 import { registerBankingModule } from './modules/banking';
 import { registerPhoneModule } from './modules/phone';
 import { registerMarketModule } from './modules/market';
-
-async function bootstrap() {
-  logger.info('Starting VELORIA RP v0.1 core...');
-  await initMySql();
-  await initRedis();
-
-  registerAccountModule();
-  registerCharacterModule();
-  registerGameplayModules();
-  registerBankingModule();
-  registerPhoneModule();
-  registerMarketModule();
-
-  mp.events.add('playerJoin', (player: PlayerMp) => {
-    player.dimension = 1000 + player.id;
-    logger.info(`Player connected: ${player.name} (${player.id})`);
-  });
-
-  mp.events.add('playerQuit', (player: PlayerMp, exitType: string, reason: string) => {
-    logger.info(`Player disconnected: ${player.name}; ${exitType}; ${reason}`);
-  });
-
-  logger.info('VELORIA RP core initialized');
-}
-
-void bootstrap().catch((error) => {
-  logger.error('Fatal bootstrap error', error);
-  process.exitCode = 1;
-});
+import { registerUiBridgeModule } from './modules/uiBridge';
+async function bootstrap(){logger.info('Starting VELORIA RP v0.1 core...');await initMySql();await initRedis();registerAccountModule();registerCharacterModule();registerGameplayModules();registerBankingModule();registerPhoneModule();registerMarketModule();registerUiBridgeModule();mp.events.add('playerJoin',(player:PlayerMp)=>{player.dimension=1000+player.id;logger.info(`Player connected: ${player.name} (${player.id})`)});mp.events.add('playerQuit',(player:PlayerMp,exitType:string,reason:string)=>logger.info(`Player disconnected: ${player.name}; ${exitType}; ${reason}`));logger.info('VELORIA RP core initialized')}
+void bootstrap().catch(error=>{logger.error('Fatal bootstrap error',error);process.exitCode=1});
